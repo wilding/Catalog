b1c01e2f2f7b323f68ee412609a43b8998a43802
wildingthrelfall <wildingthrelfall@gmail.com>
2016-02-27 08:37:20 +0000

    Refactor: dropdown menu script
    - move dropdown menu script from within the html templates to an external .js file that runs when the DOM is fully loaded

---
+++ static/dropdown_menu.js
@@ -0,0 +1,7 @@
+///////////////     PROFILE PIC DROPDOWN MENU    //////////////////////////////////////////////
+
+$(function() {
+	$(".profile_pic").on("click", function() {
+		$(".dropdown_menu").toggleClass('show_dropdown');
+	});
+});
