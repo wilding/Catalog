b1bd73df8e5661c0d0555f9c83cb0d26415281b8
wildingthrelfall <wildingthrelfall@gmail.com>
2016-02-27 01:46:25 +0000

    Refactor: responsive Github source
    - move script from within the html templates to an external .js file
    - give the <i> tag an id to make calling it more simple

---
+++ static/github_source.js
@@ -0,0 +1,20 @@
+///////////////     RESPONSIVE GITHUB SOURCE    //////////////////////////////////////////////
+
+var w = $(window);
+var d = $(document);
+var icon = $("#github_icon");
+
+d.ready(function() {
+	if (w.width() <= 475) {
+		icon.removeClass('fa-lg');
+	}
+})
+
+w.resize(function() {
+	if (w.width() <= 475) {
+		icon.removeClass('fa-lg');
+	}
+	if (w.width() > 475) {
+		icon.addClass('fa-lg');
+	}
+})
